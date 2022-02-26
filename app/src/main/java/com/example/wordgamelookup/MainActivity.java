0166062a48c2e93bc6c038ee200b0ffec967a20b
Conway <yawnoc@pm.me>
2022-02-26 18:30:28 +0800

    Make letters input field get focus on API 28+

--- app/src/main/java/com/example/wordgamelookup/MainActivity.java
+++ app/src/main/java/com/example/wordgamelookup/MainActivity.java
@@ -11,5 +11,6 @@ public class MainActivity extends AppCompatActivity
   {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.main_activity);
+    findViewById(R.id.letters_input).requestFocus();
   }
 }
