3aa3afd7bec2f5811afaf70065dadbc10e80b553
Conway <yawnoc@pm.me>
2022-02-26 23:46:59 +0800

    Normalise prefix to lowercase before lookup

--- app/src/main/java/com/example/wordgamelookup/WordEngine.java
+++ app/src/main/java/com/example/wordgamelookup/WordEngine.java
@@ -2,6 +2,7 @@ package com.example.wordgamelookup;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 import java.util.NavigableSet;
 
 public final class WordEngine
@@ -13,6 +14,12 @@ public final class WordEngine
   
   public static List<String> getWordsByPrefix(final NavigableSet<String> wordSet, final String prefix)
   {
-    return new ArrayList<>(wordSet.subSet(prefix, true, prefix + Character.MAX_VALUE, false));
+    final String prefixNormalised = normalisePrefix(prefix);
+    return new ArrayList<>(wordSet.subSet(prefixNormalised, true, prefixNormalised + Character.MAX_VALUE, false));
+  }
+  
+  private static String normalisePrefix(final String prefix)
+  {
+    return prefix.toLowerCase(Locale.ROOT);
   }
 }
