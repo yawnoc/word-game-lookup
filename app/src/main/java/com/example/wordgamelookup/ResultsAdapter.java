c38e44250c58499886011440972f6141f062df2f
Conway <yawnoc@pm.me>
2022-02-26 22:21:12 +0800

    Implement base of ResultsAdapter

---
+++ app/src/main/java/com/example/wordgamelookup/ResultsAdapter.java
@@ -0,0 +1,54 @@
+package com.example.wordgamelookup;
+
+import android.content.Context;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.List;
+
+public class ResultsAdapter extends RecyclerView.Adapter<ResultsAdapter.ResultHolder>
+{
+  private final Context context;
+  private final List<String> matchWordList;
+  
+  ResultsAdapter(final Context context, final List<String> matchWordList)
+  {
+    this.context = context;
+    this.matchWordList = matchWordList;
+  }
+  
+  @NonNull
+  @Override
+  public ResultHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int viewType)
+  {
+    final TextView resultView = new TextView(context);
+    return new ResultHolder(resultView);
+  }
+  
+  @Override
+  public void onBindViewHolder(@NonNull ResultHolder resultHolder, int resultIndex)
+  {
+    final String matchWord = matchWordList.get(resultIndex);
+    resultHolder.resultView.setText(matchWord);
+  }
+  
+  @Override
+  public int getItemCount()
+  {
+    return matchWordList.size();
+  }
+  
+  public static class ResultHolder extends RecyclerView.ViewHolder
+  {
+    private final TextView resultView;
+    
+    public ResultHolder(final TextView resultView)
+    {
+      super(resultView);
+      this.resultView = resultView;
+    }
+  }
+}
